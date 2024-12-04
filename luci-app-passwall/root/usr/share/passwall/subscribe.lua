0fe8ba70f92ddf7f253154cf771e6b15d444b51c
openwrt <openwrt@openwrt.selboo.com>
2024-12-04 17:20:11 +0800

    a
    Signed-off-by: openwrt <openwrt@openwrt.selboo.com>

--- luci-app-passwall/root/usr/share/passwall/subscribe.lua
+++ luci-app-passwall/root/usr/share/passwall/subscribe.lua
@@ -1000,10 +1000,14 @@ local function processData(szType, content, add_mode, add_from)
 				result.xhttp_path = params.path
 				result.xhttp_mode = params.mode or "auto"
 				result.xhttp_extra = params.extra
-				local Data = params.extra and params.extra ~= "" and jsonParse(params.extra)
-				local address = (Data and Data.extra and Data.extra.downloadSettings and Data.extra.downloadSettings.address)
-						or (Data and Data.downloadSettings and Data.downloadSettings.address)
-				result.download_address = address and address ~= "" and address or nil
+				local success, Data = pcall(jsonParse, params.extra)
+				if success and Data then
+					local address = (Data.extra and Data.extra.downloadSettings and Data.extra.downloadSettings.address)
+							or (Data.downloadSettings and Data.downloadSettings.address)
+					result.download_address = address and address ~= "" and address or nil
+				else
+					result.download_address = nil
+				end
 			end
 			if params.type == 'httpupgrade' then
 				result.httpupgrade_host = params.host
@@ -1212,12 +1216,19 @@ local function processData(szType, content, add_mode, add_from)
 	return result
 end
 
-local function curl(url, file, ua)
+local function curl(url, file, ua, mode)
 	local curl_args = api.clone(api.curl_args)
 	if ua and ua ~= "" and ua ~= "curl" then
 		table.insert(curl_args, '--user-agent "' .. ua .. '"')
 	end
-	local return_code, result = api.curl_logic(url, file, curl_args)
+	local return_code
+	if mode == "direct" then
+		return_code = api.curl_direct(url, file, curl_args)
+	elseif mode == "proxy" then
+		return_code = api.curl_proxy(url, file, curl_args)
+	else
+		return_code = api.curl_auto(url, file, curl_args)
+	end
 	return return_code
 end
 
@@ -1606,8 +1617,10 @@ local execute = function()
 				domain_strategy_node = domain_strategy_default
 			end
 			local ua = value.user_agent
-			log('正在订阅:【' .. remark .. '】' .. url)
-			local raw = curl(url, "/tmp/" .. cfgid, ua)
+			local access_mode = value.access_mode
+			local result = (not access_mode) and "自动" or (access_mode == "direct" and "直连访问" or (access_mode == "proxy" and "通过代理" or "自动"))
+			log('正在订阅:【' .. remark .. '】' .. url .. ' [' .. result .. ']')
+			local raw = curl(url, "/tmp/" .. cfgid, ua, access_mode)
 			if raw == 0 then
 				local f = io.open("/tmp/" .. cfgid, "r")
 				local stdout = f:read("*all")
